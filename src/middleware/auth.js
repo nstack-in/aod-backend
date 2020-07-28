9676df91e89f42ae3c31f553a24ccf19625d7ccd
Nitish Kumar Singh <nitishk72@gmail.com>
2020-07-28 16:32:23 +0530

    token added

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,12 +1,12 @@
 var jwt = require('jsonwebtoken');
 
 function authCheck(req, res, next) {
-    let token = req.headers['token'];
+    let token = req.headers['token'] || req.headers['Authorization'];
     let decoded;
     try {
         decoded = jwt.verify(token, process.env['JWT_SECRET']);
     } catch{
-        res.json({ message: "invalid token" });
+        res.status(401).json({ message: "invalid token", data: [] });
     }
     req.headers['user'] = decoded;
     next();
