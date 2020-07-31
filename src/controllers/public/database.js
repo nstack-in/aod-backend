3b26fde3edf0ac0703b811c667e7dfd180729061
Nitish Kumar Singh <nitishk72@gmail.com>
2020-07-31 14:54:29 +0530

    remove

--- src/controllers/public/database.js
+++ src/controllers/public/database.js
@@ -118,33 +118,44 @@ function updateDataController(req, res) {
 
     let filter = {
         __project__: project_id,
-        __endpoint__: endpoint_id,
         _id: document_id
     };
 
     req.body.__project__ = project_id;
     req.body.__endpoint__ = endpoint_id;
 
-    DatabaseModel.findOneAndUpdate(
-        filter,
-        req.body,
-        { new: true },
-        function (err, data) {
-            if (err) return res.json({
-                response_time: `${Date.now() - req.start}ms`,
-                data: [],
-                message: "Something went wrong",
+    Endpoint.findOne({ __project__: project_id, endpoint_id: endpoint_id }, function (err, data) {
+        if (!data)
+            return res.status(401).json({
+                message: "No Endpoint found with this name",
                 error: {
-                    message: err.message,
-                }
-            });
-            res.status(200).json({
-
-                response_time: `${Date.now() - req.start}ms`,
-                message: "Endpoint Data updated",
+                    status: true,
+                    message: "Invalid Endpoint ID"
+                },
                 data: data
             });
-        });
+        filter['__endpoint__'] = data.id;
+        DatabaseModel.findOneAndUpdate(
+            filter,
+            req.body,
+            { new: true },
+            function (err, data) {
+                if (err) return res.json({
+                    response_time: `${Date.now() - req.start}ms`,
+                    data: [],
+                    message: "Something went wrong",
+                    error: {
+                        message: err.message,
+                    }
+                });
+                res.status(200).json({
+
+                    response_time: `${Date.now() - req.start}ms`,
+                    message: "Endpoint Data updated",
+                    data: data
+                });
+            });
+    });
 }
 function findDataController(req, res) {
     let project_id = req.params.pid;
